4621c8752c267a4add6426e05df889ea3840be3b
Tony Park <tonypark0403@gmail.com>
2020-08-16 17:30:30 -0400

    fixed zero for clock

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,11 +1,19 @@
 // https://api.unsplash.com/photos/random
 
+const helper = {
+  getZero: function (value) {
+    return value < 10 ? '0' + value : value;
+  },
+};
+
 const load = {
   clock: function () {
     setInterval(function () {
       const time = new Date();
       const clock = document.querySelector('#clock');
-      clock.innerText = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+      clock.innerText = `${helper.getZero(time.getHours())}:${helper.getZero(
+        time.getMinutes()
+      )}:${helper.getZero(time.getSeconds())}`;
     }, 1000);
   },
 };
